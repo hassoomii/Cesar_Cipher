133fc9bf450288fcddd7d631fb8782cc9c390a65
hassoomii <120605011+hassoomii@users.noreply.github.com>
2024-03-27 21:10:21 -0500

    Update Cipher_file.rb

--- Cipher_file.rb
+++ Cipher_file.rb
@@ -1,1 +1,15 @@
-
+def caesar_cipher(string, string_factor)
+  encrypted_string = ''
+  string.each_char do |char|
+    if char.match(/[a-zA-Z]/)
+      uppercase = (char == char.upcase)
+      char = char.downcase
+      shifted_char = ((char.ord - 'a'.ord + string_factor) % 26 + 'a'.ord).chr
+      shifted_char = shifted_char.upcase if uppercase
+      encrypted_string += shifted_char
+    else
+      encrypted_string += char
+    end
+  end
+  return encrypted_string
+end
