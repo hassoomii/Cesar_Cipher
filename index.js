7836c4401b5ac731be3df89943a41cbecdfd447a
hassoomii <120605011+hassoomii@users.noreply.github.com>
2024-04-08 17:05:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
+// declare function
 function caesarCipher(string, factor){
+    
+    // init new string variable
     let newString = '';
+
+    // loop that checks each character (declare char variable too)
     for (let char of string) {
+
+        // checks if its a letter
         if ('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.includes(char)){
+
+            // extract code order
             codePoint = char.charCodeAt(0)
+
+            // shifts code order
             newCodePoint = codePoint + factor
+
+            // resets if it goes above letter order
             if (newCodePoint > 122){
                 newCodePoint -= 26
             }
+
+            // creates new char and adds it to empty variable
             let shiftedChar = String.fromCharCode(newCodePoint)
             newString += shiftedChar
         } else{
+
+            // adds non letter variable to string
             newString += char
         }
     }
